f5de88b8b3c63fefee239517d20d1a422a4b13f6
Bacsi Tibor <zeroxx1986@gmail.com>
2018-12-08 16:09:30 +0100

    Test login method does not require oauth defaults. Fixed.

--- test/test.go
+++ test/test.go
@@ -32,7 +32,6 @@ type EchoResponse struct {
 // This method requires authentication with 'read' permission.
 func Login(client *flickr.FlickrClient) (*LoginResponse, error) {
 	client.Init()
-	client.SetOAuthDefaults()
 	client.Args.Set("method", "flickr.test.login")
 	client.OAuthSign()
 
